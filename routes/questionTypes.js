d65c6c9edd592f8d9c60407ceb6e431b2ce87f83
belalE <belalelsiesy@gmail.com>
2021-07-17 23:51:20 -0500

    undid catchAsync

--- routes/questionTypes.js
+++ routes/questionTypes.js
@@ -3,13 +3,10 @@ const router = express.Router();
 const { QuestionType, topics } = require("../models/questionType");
 const { catchAsync } = require("../utils/catchAsync");
 
-router.get(
-  "/",
-  catchAsync(async (req, res) => {
-    const types = await QuestionType.find({});
-    res.render("questionTypes/index", { types });
-  })
-);
+router.get("/", async (req, res) => {
+  const types = await QuestionType.find({});
+  res.render("questionTypes/index", { types });
+});
 
 router.get("/new", (req, res) => {
   var topicsArr = [
@@ -21,57 +18,42 @@ router.get("/new", (req, res) => {
   res.render("questionTypes/new", { topics, topicsArr });
 });
 
-router.get(
-  "/:id",
-  catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const type = await QuestionType.findById(id).populate("questions");
-    res.render("questionTypes/show", { type });
-  })
-);
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const type = await QuestionType.findById(id).populate("questions");
+  res.render("questionTypes/show", { type });
+});
 
-router.post(
-  "/",
-  catchAsync(async (req, res) => {
-    const questionType = new QuestionType(req.body.questionType);
-    await questionType.save();
-    res.redirect(`/types/${questionType._id}`);
-  })
-);
+router.post("/", async (req, res) => {
+  const questionType = new QuestionType(req.body.questionType);
+  await questionType.save();
+  res.redirect(`/types/${questionType._id}`);
+});
 
-router.get(
-  "/:id/edit",
-  catchAsync(async (req, res) => {
-    var topicsArr = [
-      ...topics.english,
-      ...topics.math,
-      ...topics.reading,
-      ...topics.science,
-    ];
-    const { id } = req.params;
-    const type = await QuestionType.findById(id).populate("questions");
-    res.render("questionTypes/edit", { type, topicsArr, topics });
-  })
-);
+router.get("/:id/edit", async (req, res) => {
+  var topicsArr = [
+    ...topics.english,
+    ...topics.math,
+    ...topics.reading,
+    ...topics.science,
+  ];
+  const { id } = req.params;
+  const type = await QuestionType.findById(id).populate("questions");
+  res.render("questionTypes/edit", { type, topicsArr, topics });
+});
 
-router.put(
-  "/:id",
-  catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const type = await QuestionType.findByIdAndUpdate(id, {
-      ...req.body.questionType,
-    });
-    res.redirect(`/types/${type._id}`);
-  })
-);
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const type = await QuestionType.findByIdAndUpdate(id, {
+    ...req.body.questionType,
+  });
+  res.redirect(`/types/${type._id}`);
+});
 
-router.delete(
-  "/:id",
-  catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const type = await QuestionType.findByIdAndDelete(id);
-    res.redirect(`/types`);
-  })
-);
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const type = await QuestionType.findByIdAndDelete(id);
+  res.redirect(`/types`);
+});
 
 module.exports = router;
--- routes/tests.js
+++ routes/tests.js
@@ -5,107 +5,86 @@ const Question = require("../models/question");
 const { QuestionType, topics } = require("../models/questionType");
 const { catchAsync } = require("../utils/catchAsync");
 
-router.get(
-  "/",
-  catchAsync(async (req, res) => {
-    const tests = await Test.find({});
-    res.render("tests/index", { tests });
-  })
-);
+router.get(async (req, res) => {
+  const tests = await Test.find({});
+  res.render("tests/index", { tests });
+});
 
-router.get(
-  "/new",
-  catchAsync(async (req, res) => {
-    const types = await QuestionType.find({});
-    res.render("tests/new", { topics, types });
-  })
-);
+router.get("/new", async (req, res) => {
+  const types = await QuestionType.find({});
+  res.render("tests/new", { topics, types });
+});
 
-router.post(
-  "/",
-  catchAsync(async (req, res) => {
-    const test = new Test(req.body.test);
+router.post("/", async (req, res) => {
+  const test = new Test(req.body.test);
+  for (section in req.body.questions) {
+    for (
+      let i = 1;
+      i < Object.keys(req.body.questions[section]).length + 1;
+      i++
+    ) {
+      const question = new Question({
+        test: test._id,
+        index: i,
+        answer: req.body.questions[section][i.toString() - 1].answer,
+        type: req.body.questions[section][i.toString() - 1].type,
+      });
+      await question.save();
+      test.questions[section].push(question);
+    }
+  }
+  await test.save();
+  res.redirect(`/tests/${test._id}`);
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const test = await Test.findById(id);
+  res.render("tests/show", { test });
+});
+
+router.get("/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const test = await Test.findById(id)
+    .populate("questions.english")
+    .populate("questions.math")
+    .populate("questions.reading")
+    .populate("questions.science");
+  const types = await QuestionType.find({});
+  res.render("tests/edit", { test, topics, types });
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const test = await Test.findByIdAndUpdate(id, req.body.test);
     for (section in req.body.questions) {
       for (
         let i = 1;
         i < Object.keys(req.body.questions[section]).length + 1;
         i++
       ) {
-        const question = new Question({
-          test: test._id,
-          index: i,
-          answer: req.body.questions[section][i.toString() - 1].answer,
-          type: req.body.questions[section][i.toString() - 1].type,
-        });
+        const question = await Question.findByIdAndUpdate(
+          test.questions[section][i - 1],
+          {
+            answer: req.body.questions[section][i.toString() - 1].answer,
+            type: req.body.questions[section][i.toString() - 1].type,
+          }
+        );
         await question.save();
-        test.questions[section].push(question);
       }
     }
     await test.save();
     res.redirect(`/tests/${test._id}`);
-  })
-);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-router.get(
-  "/:id",
-  catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const test = await Test.findById(id);
-    res.render("tests/show", { test });
-  })
-);
-
-router.get(
-  "/:id/edit",
-  catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const test = await Test.findById(id)
-      .populate("questions.english")
-      .populate("questions.math")
-      .populate("questions.reading")
-      .populate("questions.science");
-    const types = await QuestionType.find({});
-    res.render("tests/edit", { test, topics, types });
-  })
-);
-
-router.put(
-  "/:id",
-  catchAsync(async (req, res) => {
-    try {
-      const { id } = req.params;
-      const test = await Test.findByIdAndUpdate(id, req.body.test);
-      for (section in req.body.questions) {
-        for (
-          let i = 1;
-          i < Object.keys(req.body.questions[section]).length + 1;
-          i++
-        ) {
-          const question = await Question.findByIdAndUpdate(
-            test.questions[section][i - 1],
-            {
-              answer: req.body.questions[section][i.toString() - 1].answer,
-              type: req.body.questions[section][i.toString() - 1].type,
-            }
-          );
-          await question.save();
-        }
-      }
-      await test.save();
-      res.redirect(`/tests/${test._id}`);
-    } catch (error) {
-      console.log(error);
-    }
-  })
-);
-
-router.delete(
-  "/:id",
-  catchAsync(async (req, res) => {
-    const { id } = req.params;
-    await Test.findByIdAndDelete(id);
-    res.redirect("/tests");
-  })
-);
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  await Test.findByIdAndDelete(id);
+  res.redirect("/tests");
+});
 
 module.exports = router;
