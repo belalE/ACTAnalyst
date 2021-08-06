2b0787928dcfb1bac4f4570d6f2bcad8bb1b480d
belalE <belalelsiesy@gmail.com>
2021-08-06 18:31:52 +0300

    hide from non-admin + change buttons

--- controllers/users.js
+++ controllers/users.js
@@ -6,8 +6,8 @@ module.exports.renderRegister = (req, res) => {
 
 module.exports.register = async (req, res, next) => {
   try {
-    const { email, username, password, isAdmin } = req.body;
-    const user = new User({ email, username, isAdmin });
+    const { email, username, password } = req.body;
+    const user = new User({ email, username });
     const registeredUser = await User.register(user, password);
     req.login(registeredUser, (err) => {
       if (err) return next(err);
