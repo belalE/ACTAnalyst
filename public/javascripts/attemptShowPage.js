87cd1a4fbec9b79560d14813eb05ed8b80955046
belalE <belalelsiesy@gmail.com>
2021-08-12 22:58:13 -0500

    added mistakes histogram

--- public/javascripts/attemptShowPage.js
+++ public/javascripts/attemptShowPage.js
@@ -1,3 +1,32 @@
+function getMistakeData(mistakes, section) {
+  const mistakeSection = mistakes[section];
+  const length = {
+    english: 75,
+    math: 60,
+    reading: 40,
+    science: 40,
+  }[section];
+  const indexArr = [];
+  for (let i = 0; i < mistakeSection.length; i++) {
+    indexArr.push(mistakeSection[i].index);
+  }
+  const categories = [];
+  const values = [];
+  for (let i = 5; i < length + 1; i += 5) {
+    categories.push(i - 5 + " - " + i);
+    values.push(0);
+  }
+  for (let index of indexArr) {
+    for (let i = 0; i < categories.length; i++) {
+      const min = parseInt(categories[i].slice().split(" - ")[0]);
+      const max = parseInt(categories[i].slice().split(" - ")[1]);
+      if (max >= index && index > min) {
+        values[i] += 1;
+      }
+    }
+  }
+  return { categories, values };
+}
 // https://www.w3schools.com/howto/howto_js_tabs.asp
 
 function getData(stats, section) {
@@ -242,4 +271,192 @@ Highcharts.chart("qTypeBarSci", {
   drilldown: {
     series: getData(topicStats, 3).drilldown,
   },
+});
+// TODO: Setup histogram function to enable histogram use
+Highcharts.chart("histEng", {
+  chart: {
+    type: "column",
+  },
+  title: {
+    text: "Mistake Histogram",
+  },
+  subtitle: {
+    text: "The distribution of your mistake by question #",
+  },
+  xAxis: {
+    categories: getMistakeData(mistakes, "english").categories,
+    crosshair: true,
+  },
+  yAxis: {
+    min: 0,
+    title: {
+      text: "# of Mistakes",
+    },
+  },
+  tooltip: {
+    headerFormat: '<span style="font-size:10px">Q: {point.key}</span><table>',
+    pointFormat:
+      '<tr><td style="color:{series.color};padding:0">{series.name} </td>' +
+      '<td style="padding:0"><b>{point.y} mistakes</b></td></tr>',
+    footerFormat: "</table>",
+    shared: true,
+    useHTML: true,
+  },
+  plotOptions: {
+    column: {
+      shadow: false,
+      borderWidth: 0.5,
+      borderColor: "#666",
+      pointPadding: 0,
+      groupPadding: 0,
+      color: "rgba(204,204,204,.85)",
+    },
+  },
+  series: [
+    {
+      name: "",
+      data: getMistakeData(mistakes, "english").values,
+    },
+  ],
+});
+
+Highcharts.chart("histMath", {
+  chart: {
+    type: "column",
+  },
+  title: {
+    text: "Mistake Histogram",
+  },
+  subtitle: {
+    text: "The distribution of your mistake by question #",
+  },
+  xAxis: {
+    categories: getMistakeData(mistakes, "math").categories,
+    crosshair: true,
+  },
+  yAxis: {
+    min: 0,
+    title: {
+      text: "# of Mistakes",
+    },
+  },
+  tooltip: {
+    headerFormat: '<span style="font-size:10px">Q: {point.key}</span><table>',
+    pointFormat:
+      '<tr><td style="color:{series.color};padding:0">{series.name} </td>' +
+      '<td style="padding:0"><b>{point.y} mistakes</b></td></tr>',
+    footerFormat: "</table>",
+    shared: true,
+    useHTML: true,
+  },
+  plotOptions: {
+    column: {
+      shadow: false,
+      borderWidth: 0.5,
+      borderColor: "#666",
+      pointPadding: 0,
+      groupPadding: 0,
+      color: "rgba(204,204,204,.85)",
+    },
+  },
+  series: [
+    {
+      name: "",
+      data: getMistakeData(mistakes, "math").values,
+    },
+  ],
+});
+
+Highcharts.chart("histRead", {
+  chart: {
+    type: "column",
+  },
+  title: {
+    text: "Mistake Histogram",
+  },
+  subtitle: {
+    text: "The distribution of your mistake by question #",
+  },
+  xAxis: {
+    categories: getMistakeData(mistakes, "reading").categories,
+    crosshair: true,
+  },
+  yAxis: {
+    min: 0,
+    title: {
+      text: "# of Mistakes",
+    },
+  },
+  tooltip: {
+    headerFormat: '<span style="font-size:10px">Q: {point.key}</span><table>',
+    pointFormat:
+      '<tr><td style="color:{series.color};padding:0">{series.name} </td>' +
+      '<td style="padding:0"><b>{point.y} mistakes</b></td></tr>',
+    footerFormat: "</table>",
+    shared: true,
+    useHTML: true,
+  },
+  plotOptions: {
+    column: {
+      shadow: false,
+      borderWidth: 0.5,
+      borderColor: "#666",
+      pointPadding: 0,
+      groupPadding: 0,
+      color: "rgba(204,204,204,.85)",
+    },
+  },
+  series: [
+    {
+      name: "",
+      data: getMistakeData(mistakes, "reading").values,
+    },
+  ],
+});
+
+Highcharts.chart("histSci", {
+  chart: {
+    type: "column",
+  },
+  title: {
+    text: "Mistake Histogram",
+  },
+  subtitle: {
+    text: "The distribution of your mistake by question #",
+  },
+  xAxis: {
+    categories: getMistakeData(mistakes, "science").categories,
+    crosshair: true,
+  },
+  yAxis: {
+    min: 0,
+    title: {
+      text: "# of Mistakes",
+    },
+  },
+  tooltip: {
+    headerFormat: '<span style="font-size:10px">Q: {point.key}</span><table>',
+    pointFormat:
+      '<tr><td style="color:{series.color};padding:0">{series.name} </td>' +
+      '<td style="padding:0"><b>{point.y} mistakes</b></td></tr>',
+    footerFormat: "</table>",
+    shared: true,
+    useHTML: true,
+  },
+  plotOptions: {
+    column: {
+      shadow: false,
+      borderWidth: 0.5,
+      borderColor: "#666",
+      pointPadding: 0,
+      groupPadding: 0,
+      color: "rgba(204,204,204,.85)",
+    },
+  },
+  series: [
+    {
+      name: "",
+      data: getMistakeData(mistakes, "science").values,
+    },
+  ],
 });
