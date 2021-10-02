281b8f6f290aeb1cdc99f4f1dd2ecc87a60aa998
omarkhalied <okhalid397@gmail.com>
2021-10-02 10:49:47 -0500

    Footer and Home

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const ejsLint = require("ejs-lint");
 const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 const dBUrl = process.env.DB_URL || "mongodb://localhost:27017/act-analyst";
-
 const MongoStore = require("connect-mongo");
 
 const passport = require("passport");
