d06712fe7009483106488286ada55fa0048e439e
belalE <belalelsiesy@gmail.com>
2021-10-03 09:00:39 -0500

    setup seeds

--- seeds/seedHelpers.js
+++ seeds/seedHelpers.js
@@ -1,21 +1,870 @@
-
 module.exports.forms = [
-    'D05',
-    'Z04',
-    'D03',
-    'C01',
-    'C02',
-    'C03',
-    'B03',
-    'Z15',
-    'B04',
-    'B05',
-    'A11','A09','10A','74C','74F','74H','72F','73E'
-]
+  "D05",
+  "Z04",
+  "D03",
+  "C01",
+  "C02",
+  "C03",
+  "B03",
+  "Z15",
+  "B04",
+  "B05",
+  "A11",
+  "A09",
+  "10A",
+  "74C",
+  "74F",
+  "74H",
+  "72F",
+  "73E",
+];
 
-module.exports.types = ['E', 'M','R','S'];
+module.exports.types = ["E", "M", "R", "S"];
 
 module.exports.descriptions = [
-    'April 2021',
-    'April 2021','Decemeber 2020','July 2020','June 2020','December 2019','June 2019','April 2019', 'December 2018','June 2018','April 2018','December 2017','June 2017','April 2017','December 2016','June 2016','April 2016'
+  "April 2021",
+  "April 2021",
+  "December 2020",
+  "July 2020",
+  "June 2020",
+  "December 2019",
+  "June 2019",
+  "April 2019",
+  "December 2018",
+  "June 2018",
+  "April 2018",
+  "December 2017",
+  "June 2017",
+  "April 2017",
+  "December 2016",
+  "June 2016",
+  "April 2016",
 ];
+
+module.exports.english_qts = [
+  "610d3d52cfb7933f87de329f",
+  "610d3e3bcfb7933f87de32bb",
+  "610d3e84cfb7933f87de32c1",
+  "610d3eabcfb7933f87de32c5",
+  "610d3efccfb7933f87de32cc",
+  "610d3f38cfb7933f87de32d2",
+  "610d3f62cfb7933f87de32d6",
+  "610d3d07cfb7933f87de3296",
+  "610d3d92cfb7933f87de32a7",
+  "610d3dcecfb7933f87de32af",
+  "610d3de3cfb7933f87de32b1",
+  "610d3e1bcfb7933f87de32b7",
+  "610d3e33cfb7933f87de32ba",
+  "610d3e8ccfb7933f87de32c2",
+  "610d3ee6cfb7933f87de32ca",
+  "610d3ef3cfb7933f87de32cb",
+  "610d3d8bcfb7933f87de32a6",
+  "610d3e23cfb7933f87de32b8",
+  "610d3e46cfb7933f87de32bc",
+  "610d3ea0cfb7933f87de32c4",
+  "610d3ebbcfb7933f87de32c6",
+  "610d3f27cfb7933f87de32d0",
+  "610d3d43cfb7933f87de329d",
+  "610d3da1cfb7933f87de32a9",
+  "610d3f45cfb7933f87de32d3",
+  "610d3d23cfb7933f87de3299",
+  "610d3d5ccfb7933f87de32a0",
+  "610d3d9acfb7933f87de32a8",
+  "610d3dd5cfb7933f87de32b0",
+  "610d3dfdcfb7933f87de32b4",
+  "610d3f06cfb7933f87de32cd",
+  "610d3f4ecfb7933f87de32d4",
+  "610d3f69cfb7933f87de32d7",
+  "610d3d6fcfb7933f87de32a3",
+  "610d3d0fcfb7933f87de3297",
+  "610d3d31cfb7933f87de329b",
+  "610d3d77cfb7933f87de32a4",
+  "610d3e94cfb7933f87de32c3",
+  "610d3dc7cfb7933f87de32ae",
+  "610d3e12cfb7933f87de32b6",
+  "610d3e2bcfb7933f87de32b9",
+  "610d3e4ecfb7933f87de32bd",
+  "610d3edbcfb7933f87de32c9",
+  "610d3f0fcfb7933f87de32ce",
+  "610d3e57cfb7933f87de32be",
+  "610d3dc0cfb7933f87de32ad",
+  "610d3e60cfb7933f87de32bf",
+  "610d3d2acfb7933f87de329a",
+  "610d3d4acfb7933f87de329e",
+  "610d3df4cfb7933f87de32b3",
+  "610d3d38cfb7933f87de329c",
+  "610d3d62cfb7933f87de32a1",
+  "610d3dafcfb7933f87de32ab",
+  "610d3ec6cfb7933f87de32c7",
+  "610d3f18cfb7933f87de32cf",
+  "610d3d81cfb7933f87de32a5",
+  "610d3db8cfb7933f87de32ac",
+  "610d3e09cfb7933f87de32b5",
+  "610d3ed0cfb7933f87de32c8",
+  "610d3f2ecfb7933f87de32d1",
+  "610d3d68cfb7933f87de32a2",
+  "610d3da7cfb7933f87de32aa",
+  "610d3e6acfb7933f87de32c0",
+  "610d3f57cfb7933f87de32d5",
+  "610d3d1acfb7933f87de3298",
+  "610d3deacfb7933f87de32b2",
+];
+
+module.exports.math_qts = [
+  "610d3c34cfb7933f87de3285",
+  "610d3c27cfb7933f87de3284",
+  "610d3cc2cfb7933f87de3292",
+  "610d3c79cfb7933f87de328c",
+  "610d3bf7cfb7933f87de3280",
+  "610d3bbdcfb7933f87de327c",
+  "610d3bf0cfb7933f87de327f",
+  "610d3c3ecfb7933f87de3286",
+  "610d3c10cfb7933f87de3282",
+  "610d3c53cfb7933f87de3288",
+  "610d3c5dcfb7933f87de3289",
+  "610d3be0cfb7933f87de327e",
+  "610d3c01cfb7933f87de3281",
+  "610d3c1ccfb7933f87de3283",
+  "610d3c96cfb7933f87de328e",
+  "610d3ccfcfb7933f87de3293",
+  "610d3cd7cfb7933f87de3294",
+  "610d3bcbcfb7933f87de327d",
+  "610d3c67cfb7933f87de328a",
+  "610d3c9fcfb7933f87de328f",
+  "610d3cb9cfb7933f87de3291",
+  "610d3c48cfb7933f87de3287",
+  "610d3c6fcfb7933f87de328b",
+  "610d3c88cfb7933f87de328d",
+  "610d3caccfb7933f87de3290",
+  "610d3ce2cfb7933f87de3295",
+];
+
+module.exports.reading_qts = [
+  "610d4025cfb7933f87de32ea",
+  "610d405ecfb7933f87de32f1",
+  "610d401ecfb7933f87de32e9",
+  "610d40e1cfb7933f87de32fe",
+  "610d3f7ecfb7933f87de32d9",
+  "610d4071cfb7933f87de32f4",
+  "610d3fa6cfb7933f87de32dd",
+  "610d3fdfcfb7933f87de32e3",
+  "610d3fe8cfb7933f87de32e4",
+  "610d40b6cfb7933f87de32fb",
+  "610d40eacfb7933f87de32ff",
+  "610d3faecfb7933f87de32de",
+  "610d3fefcfb7933f87de32e5",
+  "610d3ff6cfb7933f87de32e6",
+  "610d402dcfb7933f87de32eb",
+  "610d4048cfb7933f87de32ee",
+  "610d4056cfb7933f87de32f0",
+  "610d4090cfb7933f87de32f7",
+  "610d3fb8cfb7933f87de32df",
+  "610d3f85cfb7933f87de32da",
+  "610d3fcacfb7933f87de32e1",
+  "610d403fcfb7933f87de32ed",
+  "610d40c0cfb7933f87de32fc",
+  "610d3f8dcfb7933f87de32db",
+  "610d40adcfb7933f87de32fa",
+  "610d3f78cfb7933f87de32d8",
+  "610d3f9bcfb7933f87de32dc",
+  "610d406acfb7933f87de32f3",
+  "610d3fd4cfb7933f87de32e2",
+  "610d4086cfb7933f87de32f6",
+  "610d40a3cfb7933f87de32f9",
+  "610d4109cfb7933f87de3301",
+  "610d3ffecfb7933f87de32e7",
+  "610d400acfb7933f87de32e8",
+  "610d4035cfb7933f87de32ec",
+  "610d4099cfb7933f87de32f8",
+  "610d40f4cfb7933f87de3300",
+  "610d404ecfb7933f87de32ef",
+  "610d4079cfb7933f87de32f5",
+  "610d3fbfcfb7933f87de32e0",
+  "610d4064cfb7933f87de32f2",
+  "610d40d6cfb7933f87de32fd",
+];
+
+module.exports.science_qts = [
+  "61592a46d2f060126d72ae2a",
+  "61592a53d2f060126d72ae34",
+  "61592a60d2f060126d72ae3d",
+  "61592a70d2f060126d72ae47",
+  "61592a79d2f060126d72ae4e",
+  "61592a84d2f060126d72ae55",
+  "61592a8cd2f060126d72ae5c",
+  "61592a96d2f060126d72ae63",
+  "61592aa0d2f060126d72ae6a",
+  "61592aa9d2f060126d72ae71",
+  "61592ab0d2f060126d72ae78",
+  "61592ac0d2f060126d72ae82",
+  "61592acbd2f060126d72ae89",
+  "61592ad6d2f060126d72ae90",
+  "61592ae0d2f060126d72ae97",
+  "61592aebd2f060126d72ae9e",
+  "61592af3d2f060126d72aea5",
+  "61592afbd2f060126d72aeac",
+  "61592b05d2f060126d72aeb3",
+  "61592b10d2f060126d72aeba",
+  "61592b1bd2f060126d72aec1",
+  "61592b2ad2f060126d72aec8",
+  "61592b36d2f060126d72aecf",
+  "61592b40d2f060126d72aed6",
+  "61592b4ad2f060126d72aedd",
+  "61592b57d2f060126d72aee4",
+];
+
+module.exports.scales = {
+  english: [
+    {
+      scaled: 1,
+    },
+    {
+      scaled: 1,
+    },
+    {
+      scaled: 2,
+    },
+    {
+      scaled: 3,
+    },
+    {
+      scaled: 3,
+    },
+    {
+      scaled: 4,
+    },
+    {
+      scaled: 4,
+    },
+    {
+      scaled: 5,
+    },
+    {
+      scaled: 6,
+    },
+    {
+      scaled: 6,
+    },
+    {
+      scaled: 6,
+    },
+    {
+      scaled: 7,
+    },
+    {
+      scaled: 7,
+    },
+    {
+      scaled: 8,
+    },
+    {
+      scaled: 8,
+    },
+    {
+      scaled: 9,
+    },
+    {
+      scaled: 9,
+    },
+    {
+      scaled: 9,
+    },
+    {
+      scaled: 10,
+    },
+    {
+      scaled: 10,
+    },
+    {
+      scaled: 11,
+    },
+    {
+      scaled: 11,
+    },
+    {
+      scaled: 11,
+    },
+    {
+      scaled: 12,
+    },
+    {
+      scaled: 12,
+    },
+    {
+      scaled: 13,
+    },
+    {
+      scaled: 13,
+    },
+    {
+      scaled: 14,
+    },
+    {
+      scaled: 14,
+    },
+    {
+      scaled: 14,
+    },
+    {
+      scaled: 15,
+    },
+    {
+      scaled: 15,
+    },
+    {
+      scaled: 15,
+    },
+    {
+      scaled: 16,
+    },
+    {
+      scaled: 16,
+    },
+    {
+      scaled: 16,
+    },
+    {
+      scaled: 17,
+    },
+    {
+      scaled: 17,
+    },
+    {
+      scaled: 18,
+    },
+    {
+      scaled: 18,
+    },
+    {
+      scaled: 19,
+    },
+    {
+      scaled: 19,
+    },
+    {
+      scaled: 20,
+    },
+    {
+      scaled: 20,
+    },
+    {
+      scaled: 20,
+    },
+    {
+      scaled: 21,
+    },
+    {
+      scaled: 21,
+    },
+    {
+      scaled: 21,
+    },
+    {
+      scaled: 22,
+    },
+    {
+      scaled: 22,
+    },
+    {
+      scaled: 22,
+    },
+    {
+      scaled: 23,
+    },
+    {
+      scaled: 23,
+    },
+    {
+      scaled: 23,
+    },
+    {
+      scaled: 24,
+    },
+    {
+      scaled: 24,
+    },
+    {
+      scaled: 24,
+    },
+    {
+      scaled: 25,
+    },
+    {
+      scaled: 25,
+    },
+    {
+      scaled: 26,
+    },
+    {
+      scaled: 26,
+    },
+    {
+      scaled: 27,
+    },
+    {
+      scaled: 27,
+    },
+    {
+      scaled: 28,
+    },
+    {
+      scaled: 29,
+    },
+    {
+      scaled: 30,
+    },
+    {
+      scaled: 31,
+    },
+    {
+      scaled: 32,
+    },
+    {
+      scaled: 33,
+    },
+    {
+      scaled: 34,
+    },
+    {
+      scaled: 34,
+    },
+    {
+      scaled: 35,
+    },
+    {
+      scaled: 35,
+    },
+    {
+      scaled: 36,
+    },
+    {
+      scaled: 36,
+    },
+  ],
+  math: [
+    {
+      scaled: 1,
+    },
+    {
+      scaled: 4,
+    },
+    {
+      scaled: 6,
+    },
+    {
+      scaled: 8,
+    },
+    {
+      scaled: 10,
+    },
+    {
+      scaled: 11,
+    },
+    {
+      scaled: 12,
+    },
+    {
+      scaled: 12,
+    },
+    {
+      scaled: 13,
+    },
+    {
+      scaled: 13,
+    },
+    {
+      scaled: 14,
+    },
+    {
+      scaled: 14,
+    },
+    {
+      scaled: 14,
+    },
+    {
+      scaled: 15,
+    },
+    {
+      scaled: 15,
+    },
+    {
+      scaled: 15,
+    },
+    {
+      scaled: 16,
+    },
+    {
+      scaled: 16,
+    },
+    {
+      scaled: 16,
+    },
+    {
+      scaled: 16,
+    },
+    {
+      scaled: 17,
+    },
+    {
+      scaled: 17,
+    },
+    {
+      scaled: 17,
+    },
+    {
+      scaled: 18,
+    },
+    {
+      scaled: 18,
+    },
+    {
+      scaled: 19,
+    },
+    {
+      scaled: 19,
+    },
+    {
+      scaled: 20,
+    },
+    {
+      scaled: 20,
+    },
+    {
+      scaled: 21,
+    },
+    {
+      scaled: 21,
+    },
+    {
+      scaled: 22,
+    },
+    {
+      scaled: 23,
+    },
+    {
+      scaled: 23,
+    },
+    {
+      scaled: 23,
+    },
+    {
+      scaled: 24,
+    },
+    {
+      scaled: 24,
+    },
+    {
+      scaled: 25,
+    },
+    {
+      scaled: 25,
+    },
+    {
+      scaled: 25,
+    },
+    {
+      scaled: 26,
+    },
+    {
+      scaled: 26,
+    },
+    {
+      scaled: 27,
+    },
+    {
+      scaled: 27,
+    },
+    {
+      scaled: 27,
+    },
+    {
+      scaled: 27,
+    },
+    {
+      scaled: 28,
+    },
+    {
+      scaled: 28,
+    },
+    {
+      scaled: 29,
+    },
+    {
+      scaled: 29,
+    },
+    {
+      scaled: 30,
+    },
+    {
+      scaled: 30,
+    },
+    {
+      scaled: 31,
+    },
+    {
+      scaled: 32,
+    },
+    {
+      scaled: 33,
+    },
+    {
+      scaled: 34,
+    },
+    {
+      scaled: 34,
+    },
+    {
+      scaled: 35,
+    },
+    {
+      scaled: 35,
+    },
+    {
+      scaled: 36,
+    },
+  ],
+  reading: [
+    {
+      scaled: 1,
+    },
+    {
+      scaled: 2,
+    },
+    {
+      scaled: 4,
+    },
+    {
+      scaled: 5,
+    },
+    {
+      scaled: 7,
+    },
+    {
+      scaled: 8,
+    },
+    {
+      scaled: 9,
+    },
+    {
+      scaled: 10,
+    },
+    {
+      scaled: 11,
+    },
+    {
+      scaled: 11,
+    },
+    {
+      scaled: 12,
+    },
+    {
+      scaled: 13,
+    },
+    {
+      scaled: 14,
+    },
+    {
+      scaled: 14,
+    },
+    {
+      scaled: 15,
+    },
+    {
+      scaled: 16,
+    },
+    {
+      scaled: 16,
+    },
+    {
+      scaled: 17,
+    },
+    {
+      scaled: 18,
+    },
+    {
+      scaled: 19,
+    },
+    {
+      scaled: 19,
+    },
+    {
+      scaled: 20,
+    },
+    {
+      scaled: 21,
+    },
+    {
+      scaled: 22,
+    },
+    {
+      scaled: 22,
+    },
+    {
+      scaled: 23,
+    },
+    {
+      scaled: 23,
+    },
+    {
+      scaled: 24,
+    },
+    {
+      scaled: 25,
+    },
+    {
+      scaled: 26,
+    },
+    {
+      scaled: 27,
+    },
+    {
+      scaled: 28,
+    },
+    {
+      scaled: 29,
+    },
+    {
+      scaled: 30,
+    },
+    {
+      scaled: 31,
+    },
+    {
+      scaled: 32,
+    },
+    {
+      scaled: 33,
+    },
+    {
+      scaled: 34,
+    },
+    {
+      scaled: 34,
+    },
+    {
+      scaled: 35,
+    },
+  ],
+  science: [
+    {
+      scaled: 1,
+    },
+    {
+      scaled: 3,
+    },
+    {
+      scaled: 5,
+    },
+    {
+      scaled: 6,
+    },
+    {
+      scaled: 7,
+    },
+    {
+      scaled: 8,
+    },
+    {
+      scaled: 9,
+    },
+    {
+      scaled: 10,
+    },
+    {
+      scaled: 11,
+    },
+    {
+      scaled: 12,
+    },
+    {
+      scaled: 13,
+    },
+    {
+      scaled: 14,
+    },
+    {
+      scaled: 14,
+    },
+    {
+      scaled: 15,
+    },
+    {
+      scaled: 16,
+    },
+    {
+      scaled: 16,
+    },
+    {
+      scaled: 17,
+    },
+    {
+      scaled: 18,
+    },
+    {
+      scaled: 18,
+    },
+    {
+      scaled: 19,
+    },
+    {
+      scaled: 20,
+    },
+    {
+      scaled: 20,
+    },
+    {
+      scaled: 21,
+    },
+    {
+      scaled: 22,
+    },
+    {
+      scaled: 22,
+    },
+    {
+      scaled: 23,
+    },
+    {
+      scaled: 23,
+    },
+    {
+      scaled: 24,
+    },
+    {
+      scaled: 25,
+    },
+    {
+      scaled: 25,
+    },
+    {
+      scaled: 26,
+    },
+    {
+      scaled: 27,
+    },
+    {
+      scaled: 28,
+    },
+    {
+      scaled: 29,
+    },
+    {
+      scaled: 30,
+    },
+    {
+      scaled: 31,
+    },
+    {
+      scaled: 32,
+    },
+    {
+      scaled: 34,
+    },
+    {
+      scaled: 35,
+    },
+    {
+      scaled: 36,
+    },
+  ],
+};
